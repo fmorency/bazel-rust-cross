6dcae41f963f5ebc049d02d4af7e1c2197737dc9
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-03-15 13:46:43 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use openssl::rsa::Rsa;
+// use openssl::rsa::Rsa;
+use webauthn_authenticator_rs::prelude::Url;
 
 fn main() {
-    let _rsa = Rsa::generate(2048).unwrap();
+    // let _rsa = Rsa::generate(2048).unwrap();
+    let url = Url::parse("http://foo.com").unwrap();
 }
